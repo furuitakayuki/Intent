14bc7463e4117fce77af0f57d7a80d15c25edec5
furuitakayuki <maki1904@icloud.com>
2022-09-23 21:17:31 +0900

    画面遷移

--- app/src/main/java/jp/techacademy/takayuk/intent/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/intent/MainActivity.kt
@@ -2,10 +2,20 @@ package jp.techacademy.takayuk.intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
